995b4fef91737b010832c093792591c9a9901315
Gladys <gladysangelakamau@gmail.com>
2022-06-10 11:04:46 +0300

    ph1firstclassfunctadv

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driversarray) {
+    return driversarray.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function (driversarray){
+      return driversarray.slice(-2);
+  }
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const createFareMultiplier = function(x) {
+    return function(fare) {
+      return fare*x
+    }
+}
+const fareDoubler= function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler= function(fare){
+    return createFareMultiplier(3)(fare)
+}
+const selectDifferentDrivers= function(driversarray, driversToReturn){
+    return driversToReturn(driversarray);
+
+}
